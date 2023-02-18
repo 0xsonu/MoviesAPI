72a9bc6d0e77018ce727a97862923b9e9fb3e2ca
Sonu Kumar <sonu.patna0808@gmail.com>
2023-02-18 19:54:40 +0530

    Adding Movies And Review Model

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MoviesAPI
+d# MoviesAPI
 REST API Developed using Spring Boot With MongoDB Database support. (Java Project) for Practice Skills.
 
 
---
+++ src/main/java/com/project/moviesapi/Review.java
@@ -0,0 +1,19 @@
+package com.project.moviesapi;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private ObjectId id;
+    private String body;
+}
