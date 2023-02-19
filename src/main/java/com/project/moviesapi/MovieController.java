4012f02e4f2ec26bb15a482dcc8fef39afcfb541
Sonu Kumar <sonu.patna0808@gmail.com>
2023-02-19 21:40:27 +0530

    Adding findMovieByImdbId route

--- src/main/java/com/project/moviesapi/MovieController.java
+++ src/main/java/com/project/moviesapi/MovieController.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -18,5 +20,10 @@ public class MovieController {
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies() {
         return new ResponseEntity<>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable String id){
+        return new ResponseEntity<>(movieService.getMovieById(id), HttpStatus.OK);
     }
 }
--- src/main/java/com/project/moviesapi/MovieService.java
+++ src/main/java/com/project/moviesapi/MovieService.java
@@ -3,8 +3,8 @@ package com.project.moviesapi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService{
@@ -13,5 +13,9 @@ public class MovieService{
 
     public List<Movie> allMovies(){
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> getMovieById(String id) {
+        return  movieRepository.findMovieByImdbId(id);
     }
 }
